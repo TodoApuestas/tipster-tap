5789fe1f4c525a0aa1f8f558fc4c7f3f6a97e765
Mr. Brazzi <luka.ghost@gmail.com>
2014-09-11 18:15:32 -0400

    refactorizado el contenido de la pagina de detalles de configuracion del plugin

--- admin/views/admin.php
+++ admin/views/admin.php
@@ -17,6 +17,11 @@
 
 	<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
 
-	<!-- @TODO: Provide markup for your options page here. -->
+    <p><?php _e('Debes rellenar aqui las estadisticas de tus picks. Una vez lo hayas rellenado deberas elegir si permitir que a partir de ahora el calculo sea automatico (incluyendo los campos que indicamos mas abajo al crear el Pick) o manual, cambiando cada vez que lo necesites el valor de estadisticas.', Tipster_TAP::get_instance()->get_plugin_slug()) ?></p>
+    <p><?php _e('Por tanto para tener las estadisticas de los Tipster actualizadas puedes', Tipster_TAP::get_instance()->get_plugin_slug()); ?>:</p>
+    <ul>
+        <li>- <?php _e('Al crear un nuevo Pick, en el post que creais incluir en los campos que aparecen en la sección Pick (un poco mas abajo de la zona donde incluis el contenido/explicación), rellenar los campos: Categoria, Tipster, Cuota, Stake, Resultado', Tipster_TAP::get_instance()->get_plugin_slug())?></li>
+        <li>- <?php printf( __( 'Acceder a la pagina de <a href="%s">Opciones</a> y cambiar los datos, empezando por la seccion <em>Incluir datos iniciales</em> seleccionando la opcion <em>Si</em>.', Tipster_TAP::get_instance()->get_plugin_slug()), admin_url( 'admin.php?page='.Tipster_TAP::get_instance()->get_plugin_slug().'/options' )  ); ?></li>
+    </ul>
 
 </div>
